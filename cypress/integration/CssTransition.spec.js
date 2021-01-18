52f2f5bc0009a94d397e88ff6da4eeaad35cb7b1
Ben Lee <bendelonlee@gmail.com>
2021-01-18 13:32:23 -0700

    Figures out bug. Tick needed

--- cypress/integration/CssTransition.spec.js
+++ cypress/integration/CssTransition.spec.js
@@ -1,17 +1,22 @@
 describe('CssTransition', ()=>{
     it('css transition works', ()=>{
-        testCSSTranstion()
+        cy.visit('http://localhost:3000/')
+        cy.contains('Show Message').click()
+        cy.contains('This alert message is being transitioned in and out of the DOM.')
+        cy.get('.btn-primary').click()
+        cy.contains('Show Message') // This fails when clock is used. Why?
     })
-    it('css transition works when cy.clock is used', ()=>{
+    it.only('css transition works when cy.clock is used', ()=>{
         cy.clock(0)
-        testCSSTranstion()
+        cy.visit('http://localhost:3000/')
+        cy.contains('Show Message').click()
+        cy.contains('This alert message is being transitioned in and out of the DOM.')
+        cy.get('.btn-primary').click()
+        cy.tick(500)
+        cy.contains('Show Message') // This fails when clock is used. Why?
     })
 })
 
 function testCSSTranstion(){
-    cy.visit('http://localhost:3000/')
-    cy.contains('Show Message').click()
-    cy.contains('This alert message is being transitioned in and out of the DOM.')
-    cy.get('.btn-primary').click()
-    cy.contains('Show Message') // This fails when clock is used. Why?
+  
 }
